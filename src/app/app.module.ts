f1174f6afbb6524504d1f06d6da47caffe5fe3c0
nekkon <nekkon01@gmail.com>
2020-02-15 16:00:45 +0000

    Replaced default angular app code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PlaylistsComponent } from './views/playlists/playlists.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlaylistComponent } from 'src/shared/components/presentational/playlist/playlist.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PlaylistsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PlaylistsComponent, PlaylistComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
