130a7352d656f5451422f856cfb0736767c720e0
nekkon <nekkon01@gmail.com>
2020-02-15 16:02:12 +0000

    Updated featured playlists service

--- src/shared/services/playlists/playlists.service.ts
+++ src/shared/services/playlists/playlists.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IFeaturedPlaylistsDto } from './playlists.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistsService {
+  private readonly featuredPlaylistsUrl: string =
+    'https://portal.organicfruitapps.com/programming-guides/v2/us_en-us/featured-playlists.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getFeaturedPlaylists(): Observable<IFeaturedPlaylistsDto> {
+    return this.http.get<IFeaturedPlaylistsDto>(this.featuredPlaylistsUrl);
+  }
 }
