fe0095ec37d3c4690254304616c337430a41d112
nekkon <nekkon01@gmail.com>
2020-02-16 00:59:10 +0000

    Added navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { AppComponent } from './app.component';
 import { PlaylistsComponent } from './views/playlists/playlists.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PlaylistComponent } from 'src/shared/components/presentational/playlist/playlist.component';
+import { NavigationComponent } from 'src/shared/components/containers/navigation/navigation.component';
 
 @NgModule({
-  declarations: [AppComponent, PlaylistsComponent, PlaylistComponent],
+  declarations: [
+    AppComponent,
+    PlaylistsComponent,
+    PlaylistComponent,
+    NavigationComponent
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
